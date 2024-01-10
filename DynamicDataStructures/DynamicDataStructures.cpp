de0af96df2b9a869555707cf5326c2604a618612
Александр Стародубов <alexstar.97@mail.ru>
2024-01-10 22:31:32 +0300

    added constructor and destructor

--- DynamicDataStructures/DynamicDataStructures.cpp
+++ DynamicDataStructures/DynamicDataStructures.cpp
@@ -2,6 +2,69 @@
 
 using namespace std;
 
+// CUSTOM DYNAMIC DATA STRUCTURES. Это уже есть в STL, просто делаю своё.
+
+// интерфейс для всех типов для print и add добавить
+
+template<typename T>
+class List
+{
+public:
+	List();
+	~List();
+
+	void push_back(T data);
+private:
+
+	template<typename T>
+	class Node
+	{
+	public:
+		Node* pNext;
+		T data;
+		Node(T data = T(), Node* pNext = nullptr) // если не передадим адрес следующего элемента, то указываем что конец (0)
+		{									      // если не передадим данные, то вызовется конструктор того типа данных, что передали
+			this->data = data;
+			this->pNext = pNext;
+
+		}
+	};
+	int Size; 
+	Node <T>* head;
+
+};
+
+template<typename T>
+List<T>::List()
+{
+	Size = 0;
+	head = nullptr;
+}
+template<typename T>
+List<T>::~List()
+{
+
+}
+
+
+template<typename T>
+void List<T>::push_back(T data)
+{
+	if (head == nullptr)
+	{
+		head = new Node<T>(data);
+	}
+	else
+	{
+		Node<T>* current = this->head;
+
+		while (current->pNext != nullptr)
+		{
+			current = current->pNext;
+		}
+	}
+}
+
 int main()
 {
 	cout << "Enter the dynamic data structure you want to work with:\n"
@@ -12,6 +75,10 @@ int main()
 		"5 - Queue\n"
 		"6 - Priority queue\n"
 		"7 - Deque (double-ended queue)\n";
-	
+	/*List<int> lst;
+	lst.push_back(5);
+	lst.push_back(10);*/
+
+
 }
 
